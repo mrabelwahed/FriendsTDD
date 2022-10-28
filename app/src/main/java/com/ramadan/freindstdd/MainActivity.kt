2ded4b4035df4c6591d6c70668385c4e5ed85eb0
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2022-10-28 08:25:44 +0200

    setup  the project

---
+++ app/src/main/java/com/ramadan/freindstdd/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ramadan.freindstdd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ramadan.freindstdd.ui.theme.FreindsTDDTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FreindsTDDTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FreindsTDDTheme {
+        Greeting("Android")
+    }
+}
